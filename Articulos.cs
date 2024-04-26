7731156d5d5569584d1e57273c7681ecd442703c
Nicolas <nicolas.gino21@gmail.com>
2024-04-25 21:00:33 -0300

    Primera subida..

---
+++ Articulos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ejemplo_ventas
+{
+    class Telefono
+    {
+        public string numeroTelefonico {  get; set; }
+        public string modelo { get; }
+        public string marca { get; }
+
+        private int codigoOperador;
+
+        public int CodigoOperador { 
+            get { return codigoOperador; } 
+            set {if (value > 0 && value < 4)
+                    codigoOperador = value;
+                else
+                    codigoOperador = 0;
+            }
+        }
+
+        public void Llamar(string Contacto)
+        {
+            Console.WriteLine("Llamando a " + Contacto + "...");
+        }
+
+        public void Llamar() 
+        {
+            Console.WriteLine("Llamando...");
+        }
+        
+        public Telefono(string modelo, string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Ejemplo_ventas;
+using System;
+// Ventas de la semana, cada venta tiene: 
+// Codigo articulo, codigo cliente y cantidad vendida.
+// El lote corta con codigo cliente = 0.
+
+
+Telefono t1 = new Telefono("S21", "Samsung");
+t1.numeroTelefonico = "3534188541";
+Console.WriteLine("Escriba el codigo de operador: ");
+t1.CodigoOperador = int.Parse(Console.ReadLine());
+Console.WriteLine("El numero telefonico de su linea es: " + t1.numeroTelefonico + " Y el codigo de operador: " + t1.CodigoOperador);
+Console.WriteLine("La marca del celular es: " + t1.marca + " Y el modelo: " + t1.modelo);
+
+t1.Llamar("Maxi");
+
